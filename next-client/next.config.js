dd4f75be2b1fbfc0342777405cf5354ca669d1b0
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-07-29 16:54:28 -0500

    :dart: Improve: correccion next.config.js

--- next-client/next.config.js
+++ next-client/next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/:path*", // Proxy to Backend
+        destination: "http://localhost:5001/:path*", // Proxy to Backend
       },
     ];
   },
