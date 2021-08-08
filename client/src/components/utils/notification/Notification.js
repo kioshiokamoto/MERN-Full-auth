0e7b36bc3da428e72ea7af37cc15ec50fb2a3e35
Kioshi Okamoto Rojas <kjor29@gmail.com>
2021-08-07 20:04:38 -0500

    :dart: Improve: actualizar cliente e imagenes

--- client/src/components/utils/notification/Notification.js
+++
@@ -1,8 +0,0 @@
-import './notification.css';
-
-export const showErrMsg = (msg) => {
-	return <div className="errMsg">{msg}</div>;
-};
-export const showSuccessMsg = (msg) => {
-	return <div className="successMsg">{msg}</div>;
-};
